403a0e18754bf315fc2995ff5b08e2a433420fd6
prrkumar <66472963+prrkumar@users.noreply.github.com>
2020-06-05 17:25:48 +0530

    Add files via upload

---
+++ DemoConsoleApp1/DemoConsoleApp1/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoConsoleApp1
+{
+    public interface IProduct
+    {
+        string ProcessPayment();
+    }
+
+    public class PhysicalProduct : IProduct
+    {
+        public string ProcessPayment()
+        {
+            //throw new NotImplementedException("Not Implemented");
+            return ("Generate a packing slip for shipping!");
+        }
+    }
+
+    public class Book : IProduct
+    {
+        public string ProcessPayment()
+        {
+            throw new NotImplementedException("Not Implemented");
+            //return ("Generate a packing slip for shipping!");
+        }
+    }
+
+    public class NewMembership : IProduct
+    {
+        public string ProcessPayment()
+        {
+            throw new NotImplementedException("Not Implemented");
+            //return ("Generate a packing slip for shipping!");
+        }
+    }
+
+    public class UpgradeMembership : IProduct
+    {
+        public string ProcessPayment()
+        {
+            throw new NotImplementedException("Not Implemented");
+            //return ("Generate a packing slip for shipping!");
+        }
+    }
+
+    public class Video : IProduct
+    {
+        public string ProcessPayment()
+        {
+            throw new NotImplementedException("Not Implemented");
+            //return ("Generate a packing slip for shipping!");
+        }
+    }
+}
